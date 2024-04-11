abeaf3431cbc5ef8908fc3b021a428207574a4d9
kitnipan.d <kitnipan.d@gmail.com>
2024-04-11 10:16:15 +0700

    wip

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
-		}
+		adapter: adapter()
 	}
 };
 
